9bc85f383d99dc5dbc6620a06a8a7e2233188985
TaeHeon Ryoo <taroni55@naver.com>
2024-08-07 18:05:20 +0900

    [Add] BaekJoon If 2480

--- BaekJoon_CodingTest/2480/Program.cs
+++ BaekJoon_CodingTest/2480/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-namespace _2480
+﻿namespace _2480
 {
     /// <summary>
     /// 날짜 : 2024.08.07
@@ -19,20 +17,36 @@ namespace _2480
 
             int C = int.Parse(input[2]);
 
-           // if (A == B && A == C)
-           // {
-           //     int M = 10000 + A * 1000;
-           // }
-           //
-           // if (A==B || B==C || C==A)
-           // {
-           //     M = 1000 + 
-           // }
+            if (A == B && B == C)
+            {
+                A = 10000 + A * 1000;
+                Console.WriteLine(A);
+            }
 
-            
+            else if (A == B && B != C)
+            {
+                A = 1000 + A * 100;
+                Console.WriteLine(A);
+            }
+            else if (B == C && C != A)
+            {
+                B = 1000 + B * 100;
+                Console.WriteLine(B);
+            }
+            else if (C == A && A != B)
+            {
+                C = 1000 + C * 100;
+                Console.WriteLine(C);
+            }
 
-            
-
+            else if  (A != B && B != C && A != C)
+            {
+                string MAX = input.Max();
+                int max;
+                int.TryParse(MAX, out max);
+                max *= 100;
+                Console.WriteLine(max);
+            }
         }
     }
 }
