874512c8285fb8a3d9ba9dbf510683d301af860c
TaeHeon Ryoo <taroni55@naver.com>
2024-08-07 16:47:21 +0900

    [Add] BaekJoon Coding Test Sol

---
+++ BaekJoon_CodingTest/BaekJoon_CodingTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/// <summary>
+/// 날짜 : 2024.08.06 
+/// 문제 : 나머지
+/// URL : https://www.acmicpc.net/problem/10430
+/// </summary>
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+
+        int h = int.Parse(input[0]);
+    }
+}
