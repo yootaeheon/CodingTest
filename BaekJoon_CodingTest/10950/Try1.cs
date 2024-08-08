7e13f196fdd9be983ae1f5786a8da57df023c69a
TaeHeon Ryoo <taroni55@naver.com>
2024-08-08 22:19:43 +0900

    [Add] BaekJoon RepeatStatement
    Try1
    
    [Add] BaekJoon RepeatStatement Success
    
    10950 Success

---
+++ BaekJoon_CodingTest/10950/Try1.cs
@@ -0,0 +1,44 @@
+﻿//using System.Runtime.InteropServices;
+
+//namespace _10950
+//{
+//    /// <summary>
+//    /// 날짜 : 2024.08.08
+//    /// 문제 : A+B - 3
+//    /// URL : https://www.acmicpc.net/problem/10950
+//    /// </summary>
+//    internal class Try1
+//    {
+//        static void Main1(string[] args)
+//        {
+//            int T = int.Parse(Console.ReadLine());
+
+//            string[] input0 = Console.ReadLine().Split();
+//            int.TryParse(input0[0], out int A );
+//            int.TryParse(input0[1], out int B );
+           
+//            string[] input1 = Console.ReadLine().Split();
+//            int.TryParse(input1[0], out int C);
+//            int.TryParse(input1[1], out int D);
+
+//            string[] input2 = Console.ReadLine().Split();
+//            int.TryParse(input2[0], out int E);
+//            int.TryParse(input2[1], out int F);
+
+//            string[] input3 = Console.ReadLine().Split();
+//            int.TryParse(input3[0], out int G);
+//            int.TryParse(input3[1], out int H);
+
+//            string[] input4 = Console.ReadLine().Split();
+//            int.TryParse(input4[0], out int I);
+//            int.TryParse(input4[1], out int J);
+
+
+//            Console.WriteLine(A + B);
+//            Console.WriteLine(C + D);
+//            Console.WriteLine(E + F);
+//            Console.WriteLine(G + H);
+//            Console.WriteLine(I + J);
+//        }
+//    }
+//}
