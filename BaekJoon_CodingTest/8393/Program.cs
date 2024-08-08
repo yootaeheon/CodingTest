4ebbbda0aa358866c12fc891377bcb02ed7d2ce1
TaeHeon Ryoo <taroni55@naver.com>
2024-08-08 23:38:07 +0900

    [Add] BaekJoon RepeatStatement
    8393

---
+++ BaekJoon_CodingTest/8393/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _8393
+{
+    /// <summary>
+    /// 날짜 : 2024.08.08
+    /// 문제 : 합
+    /// URL : https://www.acmicpc.net/problem/8393
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
