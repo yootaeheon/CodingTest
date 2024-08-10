5a48001e507562e7ff77216c35f80e291cb1e1ae
TaeHeon Ryoo <taroni55@naver.com>
2024-08-10 23:57:24 +0900

    [Add] BaekJoon RepeatStatement

---
+++ Programmers_CodingTest/12915/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _12915
+{
+    /// <summary>
+    /// 날짜 : 2024.08.09
+    /// 문제 : 문자열 내 마음대로 정렬하기 
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12915
+    /// </summary>
+    public class Solution
+    {
+        public string[] solution(string[] strings, int n)
+        {
+            string[] answer = new string[] { };
+            answer = strings.OrderBy(x => x).OrderBy(x => x[n]).ToArray();
+            return answer;
+        }
+    }
+}
