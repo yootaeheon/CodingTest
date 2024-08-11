9cfdf5be218cc55d4e51480ed97f25d5755ef9df
TaeHeon Ryoo <taroni55@naver.com>
2024-08-11 22:52:51 +0900

    [Add] BaekJoon RepeatStatement

--- BaekJoon_CodingTest/25314/Program.cs
+++ BaekJoon_CodingTest/25314/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace _25314
 {
+    /// <summary>
+    /// 날짜 : 2024.08.11
+    /// 문제 : 코딩은 체육과목 입니다
+    /// URL : https://www.acmicpc.net/problem/25314
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int N = int.Parse(Console.ReadLine());
+            int i = N / 4;
+
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("long ");
+            }
+            Console.WriteLine("int");
         }
     }
 }
